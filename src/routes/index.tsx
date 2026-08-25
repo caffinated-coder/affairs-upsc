f753aeaae5f29e86a239ab23b2315edc27521acd
Sanjay Sarmah <49507440+caffinated-coder@users.noreply.github.com>
2026-08-25 22:31:18 +0530

    Move viewing badge to bottom-right so it does not cover language toggles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,11 @@ export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
   return (
-    <div className="relative">
-      <div className="pointer-events-none absolute right-4 top-4 z-40 sm:right-6">
-        <div className="pointer-events-auto">
-          <ViewersBadge />
-        </div>
-      </div>
+    <>
       <DigestApp />
-    </div>
+      <div className="fixed bottom-4 right-4 z-40">
+        <ViewersBadge />
+      </div>
+    </>
   );
 }
